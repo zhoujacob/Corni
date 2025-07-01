e487cda62d43f190f192b64d9e2011e3df632b8b
Jacob Zhou <zhoujaco9220@gmail.com>
2025-07-01 15:46:29 -0400

    react router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,24 +1,32 @@
 import GoogleLoginButton from '../features/auth/components/GoogleLoginButton';
 import { useAuth } from '../features/auth/AuthContext';
+import { Routes, Route } from 'react-router';
 
 function App() {
   const { accessToken } = useAuth();
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>Corni</h1>
-      {!accessToken ? (
-        <>
-          <p>Please sign in with Google to continue.</p>
-          <GoogleLoginButton />
-        </>
-      ) : (
-        <>
-          <p>✅ Logged in successfully!</p>
-          <code>{accessToken.slice(0, 40)}...</code>
-        </>
-      )}
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+            <h1>Corni</h1>
+            {!accessToken ? (
+              <>
+                <p>Please sign in with Google to continue.</p>
+                <GoogleLoginButton />
+              </>
+            ) : (
+              <>
+                <p>✅ Logged in successfully!</p>
+                <code>{accessToken.slice(0, 40)}...</code>
+              </>
+            )}
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './features/auth/AuthContext';
+import { BrowserRouter } from "react-router"
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID!}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <BrowserRouter> 
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
